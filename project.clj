c53136d940596425f79f1ec946779ab7937698bd
Rafael Delboni <rafadelboni@gmail.com>
2019-09-28 12:44:28 -0300

    fix: Query params not working #10
    Changes the `req!` function to snake_case all keywords from `:query-params`
    `req!` now accept a `options` map with `:access-key`

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject splashed "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Unsplash API Client for Clojure"
   :path "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :path "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.10.0"]
-                 [org.clojure/data.json "0.2.6"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.json "0.2.6"]
+                 [camel-snake-kebab "0.4.0"]
+                 [clj-http "3.10.0"]]
   :repl-options {:init-ns splashed.core-alpha})
